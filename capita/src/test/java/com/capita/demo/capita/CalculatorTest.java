b45f1c31e42260177d220ed023416af94533cea2
abhishekchaudhary <abhishekchaudhary@192.168.1.188>
2019-01-01 23:40:57 -0500

    adding testcases for evaliation on expression with multiple operands
    brackets and more complex testcases are still underits way

--- capita/src/test/java/com/capita/demo/capita/CalculatorTest.java
+++ capita/src/test/java/com/capita/demo/capita/CalculatorTest.java
@@ -31,18 +31,27 @@ public class CalculatorTest {
 		
 		ret = cal.convertToPostFixExpression("4 * 5 + 75 - 35");
 		assertEquals("[4, 5, *, 75, +, 35, -]", ret);
-		cal.resolveExpression();
+		ret = cal.resolveExpression();
+		assertEquals("60.0", ret);
 
-		/*ret = cal.convertToPostFixExpression("4 + 5 * 1");
+		ret = cal.convertToPostFixExpression("4 + 5 * 1");
 		assertEquals("[4, 5, 1, *, +]", ret);
+		ret = cal.resolveExpression();
+		assertEquals("9.0", ret);
 		
-		ret = cal.convertToPostFixExpression("2 - 40 / 50 + 1");
-		assertEquals("[2, 40, 50, /, -, 1, +]", ret);
+		ret = cal.convertToPostFixExpression("20 - 40 / 4 + 1");
+		assertEquals("[20, 40, 4, /, -, 1, +]", ret);
+		ret = cal.resolveExpression();
+		assertEquals("11.0", ret);
 
-		ret = cal.convertToPostFixExpression("5 * 6 / 21");
-		assertEquals("[5, 6, *, 21, /]", ret);
+		ret = cal.convertToPostFixExpression("5 * 60 / 2");
+		assertEquals("[5, 60, *, 2, /]", ret);
+		ret = cal.resolveExpression();
+		assertEquals("150.0", ret);
 		
 		ret = cal.convertToPostFixExpression("4 * 5 + 75");
-		assertEquals("[4, 5, *, 75, +]", ret);*/
+		assertEquals("[4, 5, *, 75, +]", ret);
+		ret = cal.resolveExpression();
+		assertEquals("95.0", ret);
 	}
 }
